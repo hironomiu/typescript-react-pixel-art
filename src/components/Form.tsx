dee71fdb0f084ff4fefe74df351a8701c9293496
hironomiu <hironomiu@gmail.com>
2022-03-20 08:52:47 +0900

    commited

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -10,13 +10,43 @@ type Props = {
 const Form: FC<Props> = ({ color, setColor, handleRadioChange, squareNum }) => {
   return (
     <div>
-      {' '}
-      <label className="text-xl my-4">選択</label>
-      <input
-        type="color"
-        onChange={(e) => setColor(e.target.value)}
-        value={color}
-      />
+      <div className="flex flex-row items-center">
+        {/* <label className="text-lg mx-2">選択</label> */}
+        <input
+          className="w-96 h-14"
+          type="color"
+          onChange={(e) => setColor(e.target.value)}
+          value={color}
+        />
+      </div>
+
+      <div>
+        {/* <div
+          style={{ backgroundColor: `${color}` }}
+          className="h-10 w-96"
+        ></div> */}
+        <input
+          className="w-96"
+          type="range"
+          min="0"
+          // max="255"
+          max="16777215"
+          value={parseInt(color.slice(1, 7), 16)}
+          onChange={(e) => {
+            console.log('hex:', Number(e.target.value).toString(16))
+            console.log(
+              '#' + ('000000' + Number(e.target.value).toString(16)).slice(-6)
+            )
+            console.log('10 :', parseInt(color.slice(1, 7), 16))
+            setColor(
+              '#' + ('000000' + Number(e.target.value).toString(16)).slice(-6)
+              // +
+              // ('00' + Number(e.target.value).toString(16)).slice(-2) +
+              // ('00' + Number(e.target.value).toString(16)).slice(-2)
+            )
+          }}
+        />
+      </div>
       <div>
         <label className="text-xl">
           <input
